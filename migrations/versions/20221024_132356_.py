b87e68956fd5b40006159be40ea3caee22b0828e
Willard Marett <will.j.marett@gmail.com>
2022-10-24 13:50:15 -0500

    heroku sync up

--- migrations/versions/20221024_132356_.py
+++ migrations/versions/20221024_132356_.py
@@ -1,8 +1,8 @@
-"""create_users_table
+"""empty message
 
-Revision ID: ffdc0a98111c
+Revision ID: 70da35d19b24
 Revises: 
-Create Date: 2020-11-20 15:06:02.230689
+Create Date: 2022-10-24 13:23:56.318998
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ffdc0a98111c'
+revision = '70da35d19b24'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -27,7 +27,7 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
-    # ### end Alembic commands ###qqqqqqqqq
+    # ### end Alembic commands ###
 
 
 def downgrade():
