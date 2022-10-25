87dfc83b602bbc2b63c80b4944147d6c3a00fffb
Willard Marett <will.j.marett@gmail.com>
2022-10-24 22:43:47 -0500

    updated forms

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
 
+
 class CreateReviewForm(FlaskForm):
   author_id = IntegerField('Author Id', validators=[DataRequired()])
   item_id = IntegerField('Item Id', validators=[DataRequired()])
