6ee6344439984ae196b0e1f26ca42ffdd56fe86d
Willard Marett <will.j.marett@gmail.com>
2022-10-24 22:37:52 -0500

    added shop_form

--- app/forms/item_form.py
+++ app/forms/item_form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
 
+
 class CreateItemForm(FlaskForm):
     item_name = StringField('Name', validators=[DataRequired()])
     item_price = IntegerField('Price', validators=[DataRequired()])
