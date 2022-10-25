5ac6d610c185a3b98bdfdf9c01481905f9488c6d
Willard Marett <will.j.marett@gmail.com>
2022-10-25 10:20:23 -0500

    added shop store with actions

---
+++ react-app/src/store/shop.js
@@ -0,0 +1,9 @@
+// ACTIONS:
+const GET_ALL_SHOPS = 'shop/GET_ALL_SHOPS';
+const GET_ONE_SHOP = 'shop/GET_ONE_SHOP';
+const CREATE_SHOP = 'shop/CREATE_SHOP';
+const UPDATE_SHOP = 'shop/UPDATE_SHOP';
+const DELETE_SHOP = 'shop/DELETE_SHOP';
+
+//**************************************************************************************************
+// ACTION CREATORS:
